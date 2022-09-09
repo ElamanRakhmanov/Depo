38452964bb4c93dabeb56f52ae57e02bc428c2f9
Lenovo <ElamanRakhmanov@gmail.com>
2022-09-09 21:51:56 +0300

    commit

---
+++ src/com/peaksoft/Driver.java
@@ -0,0 +1,41 @@
+package com.peaksoft;
+
+public class Driver {
+    private int id;
+    private String name;
+    private String truck;
+
+    public Driver(){
+
+    }
+
+    public Driver(int id, String name, String truck) {
+        this.id = id;
+        this.name = name;
+        this.truck = truck;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTruck() {
+        return truck;
+    }
+
+    public void setTruck(String truck) {
+        this.truck = truck;
+    }
+}
